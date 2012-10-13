a42bafd1ddf311b1572594b12d7fce0da014cf67
jleck <me@jleck.co.uk>
2012-10-13 15:01:51 +0100

    first commit

---
+++ opt/elasticbeanstalk/srv/hostmanager/lib/elasticbeanstalk/hostmanager/tasks/restartappserver.rb
@@ -0,0 +1,39 @@
+#############################################################################
+# AWS ElasticBeanstalk Host Manager
+# Copyright 2011 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Amazon Software License (the “License”). You may
+# not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+# http://aws.amazon.com/asl/
+#
+# or in the “license” file accompanying this file. This file is
+# distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+#
+
+require 'elasticbeanstalk/hostmanager/tasks/task'
+require 'elasticbeanstalk/hostmanager/utils/nginxutil'
+require 'elasticbeanstalk/hostmanager/utils/fpmutil'
+
+module ElasticBeanstalk
+  module HostManager
+    module Tasks
+
+      class RestartAppServer < Task
+        def run
+          Event.store(:nginx, 'Restarting the app server', :info, [ :milestone, :nginx ], false)
+          HostManager.log 'Restarting the app server'
+
+          ElasticBeanstalk::HostManager::Utils::FPMUtil.restart
+          ElasticBeanstalk::HostManager::Utils::NginxUtil.restart
+
+          generate_response(:deferred)
+        end
+      end # RestartAppServer class
+
+    end # Tasks module
+  end # HostManager module
+end # ElasticBeanstalk module
---
+++ opt/elasticbeanstalk/srv/hostmanager/lib/elasticbeanstalk/hostmanager/utils/fpmutil.rb
@@ -0,0 +1,67 @@
+#############################################################################
+# AWS Elastic Beanstalk Nginx/PHP-FPM Configuration
+# Copyright 2012 Carbon Coders Ltd.
+#
+# MIT LICENSE
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+module ElasticBeanstalk
+  module HostManager
+    module Utils
+
+      class FpmUtil
+        
+        def self.log(msg, *args)
+          HostManager.log msg
+          Event.store(:fpm, msg, *args)
+        end
+
+        def self.execute_fpm_cmd(verb, status_regex = /FAILED/)
+          log("Executing FPM Command: #{verb}", :info, [ :milestone, :fpm ], false)
+
+          output = `/usr/bin/sudo /etc/init.d/php-pfm #{verb}`
+
+          if ($?.exitstatus != 0 || output =~ status_regex)
+            log("FPM #{verb} FAILED", :critical, [ :fpm ])
+          else
+            log("FPM #{verb} succeeded", :info, [ :milestone, :fpm ], false)
+          end
+        end
+
+        def self.start
+          execute_fpm_cmd('start')
+        end
+
+        def self.stop
+          execute_fpm_cmd('stop')
+        end
+
+        def self.restart
+          execute_fpm_cmd('restart', /Starting FPM\: \[FAILED\]/)
+        end
+
+        def self.status
+          `/usr/bin/sudo /etc/init.d/php-fpm status`.chomp
+        end
+      end
+    end
+  end
+end
