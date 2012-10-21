0c1980b4fac0143b2d67164474f289a5886cd9b6
Dominic Rubas <github@rubas.ch>
2012-10-21 03:36:51 +0300

    Update build.sh
    PHP 5.4 , sudo fix, ImageMagick, git core

--- build.sh
+++ build.sh
@@ -1,45 +1,48 @@
 #!/bin/bash
 cd /
 
-# Update all packages
-yum -y update
+#PHP 5.5
+sudo yum -y remove httpd httpd-tools
+sudo yum -y remove php-cli php-common php 
 
-# Remove Apache
-yum -y remove httpd
+sudo yum install php54* --exclude=php54-mysqlnd 
+
+# Remove Apache - php5.4 need it
+#sudo yum -y remove httpd24 httpd24-tools
 
 # Install Nginx
-yum -y install nginx
-rm -rf /etc/nginx/conf.d/*
-chkconfig nginx off
+sudo yum -y install nginx
+sudo rm -rf /etc/nginx/conf.d/*
+sudo chkconfig nginx off
 
-# Install PHP-FPM
-yum -y install php-fpm
-chkconfig php-fpm off
+# Install PHP-FPM and PHP
+sudo yum -y install php54-fpm 
+sudo chkconfig php-fpm off
+
+
+# Update all packages
+sudo yum -y update
 
 # Overwrite files in /etc and /opt
-mkdir /tmp/build
-wget -O /tmp/build/build.tar.gz https://github.com/carboncoders/elasticbeanstalk-nginx-php/tarball/master
-tar -C /tmp/build -zxvf /tmp/build/build.tar.gz
-DIR=`find /tmp/build/carboncoders-elasticbeanstalk* -prune -type d`
-cp -rf $DIR/etc /
-cp -rf $DIR/opt /
+sudo mkdir /tmp/build
+sudo git clone git://github.com/rubas/elasticbeanstalk-nginx-php.git /tmp/build
+sudo cp -rf /tmp/build/etc /
+sudo cp -rf /tmp/build/opt /
 
 # Install Composer
-curl -s http://getcomposer.org/installer | php
-mv composer.phar /usr/bin/composer
+sudo curl -s http://getcomposer.org/installer | php
+sudo mv composer.phar /usr/bin/composer
 
 # Take ownership
-chown -R elasticbeanstalk:elasticbeanstalk /etc/nginx/conf.d \
+sudo chown -R elasticbeanstalk:elasticbeanstalk /etc/nginx/conf.d \
                                            /opt/elasticbeanstalk \
                                            /usr/bin/composer \
                                            /var/log/nginx \
                                            /var/log/php-fpm
 
-# Check for more updates
-yum -y update
 
 # Clear unneeded files
-rm -rf /etc/httpd \
+sudo rm -rf /etc/httpd \
        /opt/elasticbeanstalk/var/log/* \
        /tmp/build \
        /var/log/httpd \
