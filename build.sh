caafe437127694e1c445e7b85b06134fcbad74a4
Dominic Rubas <github@rubas.ch>
2012-10-21 14:27:11 +0300

    add sudo, install php5.4
    tested with AMI 2012.09

--- build.sh
+++ build.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
-cd /
 
-#PHP 5.5
+# Remove Apache2.2 and PHP5.3
 sudo yum -y remove httpd httpd-tools
 sudo yum -y remove php-cli php-common php 
 
+# Install PHP5.4 (with httpd2.4 as dep)
 sudo yum install php54* --exclude=php54-mysqlnd 
-
-# Remove Apache - php5.4 need it
-#sudo yum -y remove httpd24 httpd24-tools
 
 # Install Nginx
 sudo yum -y install nginx
 sudo rm -rf /etc/nginx/conf.d/*
 sudo chkconfig nginx off
 
-# Install PHP-FPM and PHP
+# Install PHP-FPM
 sudo yum -y install php54-fpm 
 sudo chkconfig php-fpm off
-
 
 # Update all packages
 sudo yum -y update
@@ -30,6 +26,7 @@ sudo cp -rf /tmp/build/etc /
 sudo cp -rf /tmp/build/opt /
 
 # Install Composer
+cd /tmp/
 sudo curl -s http://getcomposer.org/installer | php
 sudo mv composer.phar /usr/bin/composer
 
@@ -50,4 +47,4 @@ sudo rm -rf /etc/httpd \
        /var/log/php-fpm/*
 
 # Clear root history
-history -c
+sudo history -c
